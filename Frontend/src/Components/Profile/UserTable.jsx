44461af6fe414db70e8d8698604bd56c76a84ffc
Nanoray22-dev <raysellconcepcionpaulino@gmail.com>
2024-05-08 11:30:25 -0400

    filtered user

--- Frontend/src/Components/Profile/UserTable.jsx
+++ Frontend/src/Components/Profile/UserTable.jsx
@@ -3,11 +3,11 @@ import axios from "axios";
 import EditUserForm from "./UserEditForm";
 import Navigation from "../Home/Navigation";
 
-
 const UserTable = () => {
   const [users, setUsers] = useState([]);
   const [editUserId, setEditUserId] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -22,8 +22,6 @@ const UserTable = () => {
     fetchUsers();
   }, []);
 
-
-  
   const openModal = (userId) => {
     setEditUserId(userId);
     setIsModalOpen(true);
@@ -34,51 +32,88 @@ const UserTable = () => {
     setIsModalOpen(false);
   };
 
+  const filteredUsers = users.filter((user) =>
+    user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <>
-    <Navigation   />
-    <div className="container mx-auto">
-      <h2 className="text-2xl font-bold mb-4">Lista de Usuarios</h2>
-      <table className="min-w-full">
-        <thead>
-          <tr>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Username</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Email</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Address</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Phone</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Age</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Residence Type</th>
-            <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Role</th>
-
-            <th className="px-6 py-3 bg-gray-50"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id} className="border-t border-gray-200">
-              <td className="px-6 py-4 whitespace-no-wrap">{user.username}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.email}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.address}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.phone}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.age}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.residenceType}</td>
-              <td className="px-6 py-4 whitespace-no-wrap">{user.role}</td>
-
-
-              <td className="px-6 py-4 whitespace-no-wrap">
-                <button onClick={() => openModal(user._id)} className="text-indigo-600 hover:text-indigo-900">Editar</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {isModalOpen && <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
-        <div className="bg-white p-6 rounded-lg">
-          <button onClick={closeModal} className="absolute  mb-24  ">Cerrar</button>
-          <EditUserForm userId={editUserId} closeModal={closeModal}/>
+      <Navigation />
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Lista de Usuarios</h2>
+        <div>
+          <input type="text"
+          placeholder="Buscar por username"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-gray-300 rounded-md px-4 py-2"
+          />
         </div>
-      </div>}
-    </div>
+        <table className="min-w-full">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Username
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Email
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Address
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Phone
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Age
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Residence Type
+              </th>
+              <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                Role
+              </th>
+
+              <th className="px-6 py-3 bg-gray-50"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredUsers.map((user) => (
+              <tr key={user._id} className="border-t border-gray-200">
+                <td className="px-6 py-4 whitespace-no-wrap">
+                  {user.username}
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap">{user.email}</td>
+                <td className="px-6 py-4 whitespace-no-wrap">{user.address}</td>
+                <td className="px-6 py-4 whitespace-no-wrap">{user.phone}</td>
+                <td className="px-6 py-4 whitespace-no-wrap">{user.age}</td>
+                <td className="px-6 py-4 whitespace-no-wrap">
+                  {user.residenceType}
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap">{user.role}</td>
+
+                <td className="px-6 py-4 whitespace-no-wrap">
+                  <button
+                    onClick={() => openModal(user._id)}
+                    className="text-indigo-600 hover:text-indigo-900"
+                  >
+                    Editar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75">
+            <div className="bg-white p-6 rounded-lg">
+              <button onClick={closeModal} className="absolute  mb-24  ">
+                Cerrar
+              </button>
+              <EditUserForm userId={editUserId} closeModal={closeModal} />
+            </div>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- Frontend/src/Components/Report/CreateReport.jsx
+++ Frontend/src/Components/Report/CreateReport.jsx
@@ -11,7 +11,7 @@ const CreateReport = ({ onSubmit, onCloseModal }) => {
   const [reports, setReports] = useState([]);
   const [selectedState] = useState("PENDING");
 
-  const handleSubmit = async ({ onCloseModal }) => {
+  const handleSubmit = async () => {
     try {
       const formData = new FormData();
       formData.append("title", title);
@@ -41,7 +41,6 @@ const CreateReport = ({ onSubmit, onCloseModal }) => {
       setTitle("");
       setDescription("");
       setImage(null);
-      onCloseModal();
       setIncidentDate("");
 
       onSubmit();
@@ -52,105 +51,125 @@ const CreateReport = ({ onSubmit, onCloseModal }) => {
     }
   };
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]; // Solo toma el primer archivo seleccionado
+    setImage(file);
+  };
+
+  // const handleImageRemove = (index) => {
+  //   const newImages = [...image];
+  //   newImages.splice(index, 1);
+  //   setImage(newImages);
+  // };
+
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center overflow-x-auto overflow-y-auto bg-black bg-opacity-50">
-      <div className="flex justify-center items-center h-screen">
-        <div className="bg-white  rounded-lg shadow-lg p-24 w-full max-w-lg">
-          <h2 className="text-2xl font-semibold mb-8 mr-8">Crear Reporte</h2>
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            className="space-y-4 p-4 mb-4 px-8"
-          >
-            <div>
-              <label
-                htmlFor="title"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Título:
-              </label>
-              <input
-                type="text"
-                id="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="description"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Descripción:
-              </label>
-              <textarea
-                id="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                required
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              ></textarea>
-            </div>
-            <div>
-              <label
-                htmlFor="image"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Imagen:
-              </label>
-              <div className="mt-1 flex justify-between items-center  rounded-md shadow-sm">
-                <label
-                  htmlFor="image"
-                  className="cursor-pointer bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      <div className="bg-white rounded-lg shadow-lg w-full max-w-lg">
+        <h2 className="text-2xl font-semibold mb-8 text-center">
+          Crear Reporte
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4 p-4 mb-4 px-8">
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Título:
+            </label>
+            <input
+              className="title bg-gray-100 border border-gray-300 p-2 mb-4 w-full rounded-md outline-none"
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Descripción:
+            </label>
+            <textarea
+              className="description bg-gray-100 sec p-3 h-32 border border-gray-300 rounded-md outline-none min-w-full"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              placeholder="Describe todo sobre este reporte aquí"
+            ></textarea>
+          </div>
+          <div>
+            <div className="mt-1 flex justify-between items-center rounded-md shadow-sm">
+              <label id="select-image">
+                <svg
+                  className="mr-2 cursor-pointer hover:text-gray-700 border rounded-full p-1 h-7"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  Seleccionar Archivo
-                </label>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                  />
+                </svg>
                 <input
                   id="image"
                   type="file"
-                  accept="image"
-                  onChange={(e) => setImage(e.target.files[0])}
-                  multiple
+                  accept="image/*"
+                  onChange={handleImageChange}
                   className="hidden"
                 />
-              </div>
-            </div>
-
-              <p>Se han seleccionado {image.length } imágenes.</p>
-
-            <div>
-              <label
-                htmlFor="incidentDate"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Fecha de la incidencia:
               </label>
-              <input
-                type="date"
-                id="incidentDate"
-                value={incidentDate}
-                onChange={(e) => setIncidentDate(e.target.value)}
-                required
-                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              <div className="count ml-auto text-gray-400 text-xs font-semibold">0/300</div>
+            </div>
+          </div>
+          {image && image instanceof File && (
+            <div className="flex items-center">
+              <img
+                src={URL.createObjectURL(image)}
+                alt="Vista previa de la imagen"
+                className="w-20 h-20 object-cover rounded-md"
               />
+              <p className="ml-2 text-sm text-gray-600">{image.name}</p>
             </div>
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="bg-indigo-600 text-white px-4 py-2 rounded-lg mr-2 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-              >
-                Crear Reporte
-              </button>
-              <button
-                onClick={onCloseModal}
-                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-              >
-                Cerrar
-              </button>
-            </div>
-          </form>
-        </div>
+          )}
+          <div>
+            <label
+              htmlFor="incidentDate"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Fecha de la incidencia:
+            </label>
+            <input
+              type="date"
+              id="incidentDate"
+              value={incidentDate}
+              onChange={(e) => setIncidentDate(e.target.value)}
+              required
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-indigo-600 text-white px-4 py-2 rounded-lg mr-2 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+            >
+              Crear Reporte
+            </button>
+            <button
+              onClick={onCloseModal}
+              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+            >
+              Cerrar
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
